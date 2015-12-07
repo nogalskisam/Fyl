3051e7055d877c53c0a02c419b6284dae6a8ffe7
Samuel Nogalski <samuel.nogalski@thebigword.com>
2015-12-07 15:56:33 +0000

    Broken db connection, add some other basics.

---
+++ Tenant.Site/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tenant.Service;
+
+namespace Tenant.Site.Controllers
+{
+    public class UserController : Controller
+    {
+        private ITenantService _tenantService;
+
+        public UserController(ITenantService tenantService)
+        {
+            _tenantService = tenantService;
+        }
+
+
+    }
+}
